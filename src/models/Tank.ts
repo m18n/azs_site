5c83d9c8f3acd162c60f4eab521fb780043b1325
front-end dev <noreply@front-end.dev>
2024-02-28 18:55:04 +0100

    feat: add api call

---
+++ src/models/Tank.ts
@@ -0,0 +1,6 @@
+export interface Tank {
+  id_tank: number
+  id_tovar: number
+  volume: number
+  remain: number
+}
---
+++ src/services/providers/api.ts
@@ -0,0 +1,11 @@
+const baseURL = import.meta.env.VITE_BASE_URL
+
+const api = {
+  async get<T>(url: string): Promise<T> {
+    const response = await fetch(`${baseURL}/api${url}`)
+    const data = await response.json()
+    return data as T
+  },
+}
+
+export default api
